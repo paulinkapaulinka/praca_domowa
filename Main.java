f3c517763c68f7d54f0ba451ac8801d4322deb44
paulinkapaulinka <paulinka.ruta@gmail.com>
2022-08-18 18:47:26 +0200

    nauka zmiennych w javie

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hej Paulina");
-        System.out.println("super grupa");
+        String name = "Paulina";
+        int age = 36;
+        char znak = 'a';
+        int x = 10;
+        int y = 20;
+        int c = x+y;
+
+        System.out.println(c);
+        //System.out.println("Hej "+name + ", ktora ma " +age + " lat.");
+
+
+
+
     }
 }
